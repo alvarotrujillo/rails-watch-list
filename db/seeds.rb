f92c71fcadc020e43c85f876f2d084b6d02f6dcf
Alvaro Trujillo <alvaro.trujillo@me.com>
2022-08-18 13:05:28 +0200

    API with Movie Database is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts "Clearing database..."
+Movie.destroy_all
+
+"Loading up database"
+url = "https://tmdb.lewagon.com/movie/top_rated"
+response = URI.open(url).read
+result = JSON.parse(response)
+
+array = result["results"]
+p array[0]["title"]
+
+array.each_with_index do|item, i|
+  movies = Movie.new(
+    title: item["title"],
+    overview: item["overview"],
+    poster_url: item["poster_path"],
+    rating: item["vote_average"]
+  )
+  movies.save!
+end
+
+puts "done"
